f9b594304360c11395ee8d5c0aecc34a96029ca9
Asadul Islam Hamzah <68386760+asad-cuet@users.noreply.github.com>
2024-01-17 15:26:35 +0600

    after login updated

--- app/Http/Livewire/HomeComponent.php
+++ app/Http/Livewire/HomeComponent.php
@@ -3,11 +3,50 @@
 namespace App\Http\Livewire;
 use App\Models\ServiceCategory;
 use Livewire\Component;
+use Auth;
+use App\Models\Order;
 
 class HomeComponent extends Component
 {
+    public $history;
+    public function orderHistory()
+    {
+        $this->history=1;
+    }
+    public function orderNew()
+    {
+        $this->history=0;
+    }
     public function render()
     {
+        if(Auth::user()!='')
+        {
+            if(Auth::user()->utype=='CST')
+            {
+                $orders=Order::where('user_id',Auth::user()->id)->orderBy('id','desc')->get();
+                return view('livewire.customer.customer-dashboard-component',['orders'=>$orders])->layout('layouts.base');
+            }
+            else if(Auth::user()->utype=='SVP')
+            {
+                $orders=Order::where('to_provider_id',Auth::user()->id)->orderBy('id','desc')->get(); 
+                return view('livewire.sprovider.sprovider-dashboard-component',['orders'=>$orders])->layout('layouts.base');
+            }
+            else if(Auth::user()->utype=='ADM')
+            {
+                if($this->history==1)
+                {
+                    $orders=Order::where('order_completed',1)->orderBy('updated_at','desc')->get(); 
+                }
+                else
+                {
+                    $orders=Order::where('order_completed',0)->orderBy('id','desc')->get(); 
+                }
+                
+                return view('livewire.admin.order-details-component',['orders'=>$orders])->layout('layouts.base');
+                // return redirect(route('admin.new-order'));
+            }
+        }
+
         $fscategories = ServiceCategory::where('featured',1)->take(8)->get();
         return view('livewire.home-component',['fscategories' => $fscategories])->layout('layouts.base');
     }
